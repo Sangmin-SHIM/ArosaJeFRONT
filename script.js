e0dd27010ff4cd2b2afa26c4d17da88044d19681
sangmin <tkdals4723@gmail.com>
2023-05-30 15:42:02 +0200

    publish / unpublish button

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $('.navTrigger').click(function () {
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const arrowBtns = document.querySelectorAll(".wrapper i");
+const arrowBtns = document.querySelectorAll(".wrapper i.arrow");
 const carouselChildrens = [...carousel.children];
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 // Get the number of cards that can fit in the carousel at once

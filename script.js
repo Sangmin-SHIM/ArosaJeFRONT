59cbfa9b4ee1a4019c07e879e4c4475deb80c547
sangmin <tkdals4723@gmail.com>
2023-05-30 16:35:40 +0200

    send to mail by JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 // --------------------------------------------------------------
 // Position Fix
 // --------------------------------------------------------------
@@ -29,21 +30,16 @@ const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i.arrow");
 const carouselChildrens = [...carousel.children];
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
-// Get the number of cards that can fit in the carousel at once
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
-// Insert copies of the first few cards to end of carousel for infinite scrolling
 carouselChildrens.slice(0, cardPerView).forEach(card => {
     carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
 carousel.classList.add("no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("no-transition");
-// Add event listeners for the arrow buttons to scroll the carousel left and right
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
@@ -52,13 +48,11 @@ arrowBtns.forEach(btn => {
 const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
     startX = e.pageX;
     startScrollLeft = carousel.scrollLeft;
 }
 const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
+    if(!isDragging) return; 
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 }
 const dragStop = () => {
@@ -66,25 +60,21 @@ const dragStop = () => {
     carousel.classList.remove("dragging");
 }
 const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
     if(carousel.scrollLeft === 0) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
         carousel.classList.remove("no-transition");
     }
-    // If the carousel is at the end, scroll to the beginning
     else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("no-transition");
     }
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
     clearTimeout(timeoutId);
     if(!wrapper.matches(":hover")) autoPlay();
 }
 const autoPlay = () => {
-    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
+    if(window.innerWidth < 800 || !isAutoPlay) return; 
     timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
 }
 autoPlay();
@@ -99,33 +89,29 @@ wrapper.addEventListener("mouseleave", autoPlay);
 // Modal - Send a message
 // --------------------------------------------------------------
 const sendMessageModal = document.getElementById('sendMessageModal')
+var messageTextArea = document.getElementById('message-text');
 sendMessageModal.addEventListener('show.bs.modal', event => {
-  // Button that triggered the modal
   const button = event.relatedTarget
-  // Extract info from data-bs-* attributes
   const recipient = button.getAttribute('data-bs-to-owner')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  const modalTitle = sendMessageModal.querySelector('.modal-title')
 
+  const modalTitle = sendMessageModal.querySelector('.modal-title')
   modalTitle.textContent = `New message to ${recipient}`
+  messageTextArea.value = "";
 })
+
+function sendMail() {
+    var message = document.getElementById("message-text").value;
+    var mailtoLink = "mailto:data-bs-to-owner?subject=Subject auto&body=" + encodeURIComponent(message);
+    window.location.href = mailtoLink;
+}
 
 // --------------------------------------------------------------
 // Modal - Upload
 // --------------------------------------------------------------
 const uploadModal = document.getElementById('uploadModal')
 uploadModal.addEventListener('show.bs.modal', event => {
-  // Button that triggered the modal
   const button = event.relatedTarget
-  // Extract info from data-bs-* attributes
   const owner = button.getAttribute('data-bs-owner-id')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
   const modalTitle = uploadModal.querySelector('.modal-title')
 
   modalTitle.textContent = `Uploader les plantes (${owner})`
@@ -142,15 +128,11 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 // --------------------------------------------------------------
 function readFile() {
     if (!this.files || !this.files[0]) return;
-      
     const FR = new FileReader();
-      
     FR.addEventListener("load", function(evt) {
       document.querySelector("#img").src         = evt.target.result;
     }); 
-      
     FR.readAsDataURL(this.files[0]);
-    
   }
   
   document.querySelector("#inp").addEventListener("change", readFile);
